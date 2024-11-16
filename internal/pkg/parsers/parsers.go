10788c9f53702190ec4ebfa690f2b8a8c2817124
ferdie bergado <ferdiebergado@gmail.com>
2024-11-16 11:36:50 +0800

    Renamed ParseTimeout to ParseInt

--- internal/pkg/parsers/parsers.go
+++ internal/pkg/parsers/parsers.go
@@ -2,8 +2,8 @@ package parsers
 
 import "strconv"
 
-// Helper to parse timeout values with a default
-func ParseTimeout(value string, defaultValue int) int {
+// Helper to parse integer values with a default
+func ParseInt(value string, defaultValue int) int {
 	timeout, err := strconv.Atoi(value)
 
 	if err != nil {
