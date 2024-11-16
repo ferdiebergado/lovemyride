844213ab10467ccd831fabd7a6e540562e25d1c8
ferdie bergado <ferdiebergado@gmail.com>
2024-11-16 11:37:37 +0800

    Refactored to use environment variables as function parameter

--- internal/pkg/db/db.go
+++ internal/pkg/db/db.go
@@ -3,27 +3,28 @@ package db
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	"github.com/ferdiebergado/lovemyride/internal/pkg/logger"
+	"github.com/ferdiebergado/lovemyride/internal/pkg/parsers"
 )
 
 const (
-	dbDriver           = "pgx"
 	connMaxLifeTime    = 0
 	maxIdleConnections = 50
 	maxOpenConnections = 50
 )
 
-func Connect(_ context.Context, dsn string) *sql.DB {
-	db, err := sql.Open(dbDriver, dsn)
+func Connect(ctx context.Context, getenv func(string) string) *sql.DB {
+	db, err := sql.Open("pgx", getenv("DATABASE_URL"))
 
 	if err != nil {
 		logger.Fatal("Unable to connect to database", err)
 	}
 
-	db.SetConnMaxLifetime(connMaxLifeTime)
-	db.SetMaxIdleConns(maxIdleConnections)
-	db.SetMaxOpenConns(maxOpenConnections)
+	db.SetConnMaxLifetime(time.Duration(parsers.ParseInt(getenv("DB_CONN_MAX_LIFETIME"), connMaxLifeTime)))
+	db.SetMaxIdleConns(parsers.ParseInt(getenv("DB_MAX_IDLE_CONNECTIONS"), maxIdleConnections))
+	db.SetMaxOpenConns(parsers.ParseInt(getenv("DB_MAX_OPEN_CONNECTIONS"), maxOpenConnections))
 
 	return db
 }
