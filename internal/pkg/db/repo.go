6d17998efd1059c01e66137fe30fe3655bfa0502
ferdie bergado <ferdiebergado@gmail.com>
2024-11-17 20:45:10 +0800

    Removed pointers

--- internal/pkg/db/repo.go
+++ internal/pkg/db/repo.go
@@ -3,11 +3,11 @@ package db
 import "context"
 
 type Repository[T any] interface {
-	Create(ctx context.Context, params any) (*T, error)
-	Find(ctx context.Context, id string) (*T, error)
+	Create(ctx context.Context, params any) (T, error)
+	Find(ctx context.Context, id string) (T, error)
 	Update(ctx context.Context, params any) error
 	Delete(ctx context.Context, id string) error
-	FindAll(ctx context.Context, filter any) ([]*T, error)
+	FindAll(ctx context.Context, filter any) ([]T, error)
 }
 
 type Finder[T any] interface {
