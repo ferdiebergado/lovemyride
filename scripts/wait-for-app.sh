3892009af5fb9892ca7c8da23cab9dcea8ebeb0e
ferdie bergado <ferdiebergado@gmail.com>
2024-11-17 19:49:30 +0800

    Used env var for the port in the script

--- scripts/wait-for-app.sh
+++ scripts/wait-for-app.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Wait for the app to become ready
-until curl -s http://app:8000/health; do
-	echo "Waiting for app..."
+until curl -s http://app:$PORT/health; do
+	echo "Waiting for app on port: $PORT..."
 	sleep 2
 done
 
